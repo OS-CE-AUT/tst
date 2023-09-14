ce0775f9272944330ae0633b891324ac48f7397e
amirfazel <amirfazel45@gmail.com>
2023-09-14 16:19:47 +0330

    uding cdn for library

--- assets/js/calendar.js
+++ assets/js/calendar.js
@@ -3,12 +3,12 @@ console.log('calendar working')
 // const yaml = require('js-yaml');
 // const fs   = require('fs');
 
-import * as yaml from 'js-yaml';
-import * as fs from 'fs';
+// import * as yaml from 'js-yaml';
+// import * as fs from 'fs';
 
 // Get document, or throw exception on error
 try {
-  const doc = yaml.load(fs.readFileSync('../../_data/calendar.yml'), 'utf8');
+  const doc = jsyaml.load('../../_data/calendar.yml');
   console.log(doc)
 } catch (e) {
   console.log(e);
