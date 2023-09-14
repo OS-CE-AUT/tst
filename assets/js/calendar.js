be7a48ac4d5d2ff08f88b9b3f9f770ba062c1a88
amirfazel <amirfazel45@gmail.com>
2023-09-14 16:40:34 +0330

    cdn's and logic updated

--- assets/js/calendar.js
+++ assets/js/calendar.js
@@ -7,9 +7,22 @@ console.log('calendar working')
 // import * as fs from 'fs';
 
 // Get document, or throw exception on error
-try {
-  const doc = jsyaml.load(fs.readFileSync('../../_data/calendar.yml'), 'utf8');
-  console.log(doc)
-} catch (e) {
-  console.log(e);
-}
+// try {
+//   const doc = jsyaml?.load(fs?.readFileSync('../../_data/calendar.yml'), 'utf8');
+//   console.log(doc)
+// } catch (e) {
+//   console.log(e);
+// }
+
+fetch('../../_data/calendar.yml')
+  .then(response => response.text())
+  .then(yamlData => {
+    // Parse the YAML data
+    const doc = jsyaml.load(yamlData);
+
+    // Now you can work with the parsed YAML data
+    console.log(doc);
+  })
+  .catch(error => {
+    console.error('Error fetching YAML data:', error);
+  });
