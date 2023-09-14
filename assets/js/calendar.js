9a6962dff695aa8e313e460289c548db9efc26f9
amirfazel <amirfazel45@gmail.com>
2023-09-14 13:58:23 +0330

    testing library

--- assets/js/calendar.js
+++ assets/js/calendar.js
@@ -1,1 +1,12 @@
 console.log('calendar working')
+
+const yaml = require('js-yaml');
+const fs   = require('fs');
+
+// Get document, or throw exception on error
+try {
+  const doc = yaml.load(fs.readFileSync('../../_data/calendar.yml'), 'utf8');
+  console.log('doc: ', doc);
+} catch (e) {
+  console.log(e);
+}
