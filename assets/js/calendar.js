748f8210380b08a1cdbc6778e3180a5613d169d5
amirfazel <amirfazel45@gmail.com>
2023-09-15 12:52:20 +0330

    logic initiated

--- assets/js/calendar.js
+++ assets/js/calendar.js
@@ -1,28 +1,4 @@
 console.log('calendar working')
 
-// const yaml = require('js-yaml');
-// const fs   = require('fs');
-
-// import * as yaml from 'js-yaml';
-// import * as fs from 'fs';
-
-// Get document, or throw exception on error
-// try {
-//   const doc = jsyaml?.load(fs?.readFileSync('../../_data/calendar.yml'), 'utf8');
-//   console.log(doc)
-// } catch (e) {
-//   console.log(e);
-// }
-
-// fetch('../../_data/calendar.yml')
-//   .then(response => response.text())
-//   .then(yamlData => {
-//     // Parse the YAML data
-//     const doc = jsyaml.load(yamlData);
-
-//     // Now you can work with the parsed YAML data
-//     console.log(yamlData, doc);
-//   })
-//   .catch(error => {
-//     console.error('Error fetching YAML data:', error);
-//   });
+const months = document.querySelectorAll('.month');
+console.log(months)
