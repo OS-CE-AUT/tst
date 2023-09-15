fb81e294ebfeae30c07a8796d4270362cdca6139
amirfazel <amirfazel45@gmail.com>
2023-09-15 13:53:12 +0330

    js testing

--- assets/js/calendar.js
+++ assets/js/calendar.js
@@ -2,16 +2,20 @@ document.onload = function() {
     console.log('calendar working')
 
 const months = document.querySelectorAll("[data-type='month']");
+const buttons = document.querySelectorAll("data-type='month-button'")
+    
+console.log(months)
+console.log(buttons)
 
-    months[0].style.display = 'block';
+// months[0].style.display = 'block';
 
-const showMonth = (name) => {
+// const showMonth = (name) => {
 
-    months.forEach(month => {month.style.display = 'none';});
+//     months.forEach(month => {month.style.display = 'none';});
 
 
-    let month = document.querySelector(`[data-value=${name}]`)
-    month.style.display = 'block'
-}
+//     let month = document.querySelector(`[data-value=${name}]`)
+//     month.style.display = 'block'
+// }
 
 }
