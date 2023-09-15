e90bd82b62968f30c4b7debd2b2dc2302ca53f72
amirfazel <amirfazel45@gmail.com>
2023-09-15 13:45:09 +0330

    applying initial logic

--- assets/js/calendar.js
+++ assets/js/calendar.js
@@ -2,6 +2,16 @@ document.onload = function() {
     console.log('calendar working')
 
 const months = document.querySelectorAll("[data-type='month']");
-console.log(months)
+
+    months[0].style.display = 'block';
+
+const showMonth = (name) => {
+
+    months.forEach(month => {month.style.display = 'none';});
+
+
+    let month = document.querySelector(`[data-value=${name}]`)
+    month.style.display = 'block'
+}
 
 }
