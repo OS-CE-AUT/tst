6f3159812bf5a17ced3d202968bc8448f478e85e
amirfazel <amirfazel45@gmail.com>
2023-09-15 16:56:50 +0330

    removed logs

--- assets/js/calendar.js
+++ assets/js/calendar.js
@@ -1,17 +1,11 @@
-
-    console.log('calendar working')
-
 const months = document.querySelectorAll("[data-type='month']");
 const buttons = document.querySelectorAll("[data-type='monthButton']")
-    
-console.log(months)
-console.log(buttons)
+
 
 months.forEach(month => {month.style.display = 'none';});
 months[0].style.display = 'block';
 
 const showMonth = (name) => {
-    console.log('button clicked month:', name)
 
     months.forEach( month => {if (month.dataset.month == name) {month.style.display = 'block'} else {month.style.display='none'}})
 }
