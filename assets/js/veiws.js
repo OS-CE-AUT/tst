53fbfda1e651721430de05e6d2be785bf18b35e3
amirfazel <amirfazel45@gmail.com>
2023-10-06 13:54:36 +0330

    spamming logs

--- assets/js/veiws.js
+++ assets/js/veiws.js
@@ -1,4 +1,5 @@
 import { getAnalytics, logEvent } from "firebase/analytics";
-
+console.log('views')
 const analytics = getAnalytics();
+console.log('logEvent', logEvent);
 logEvent(analytics, 'page_view');
